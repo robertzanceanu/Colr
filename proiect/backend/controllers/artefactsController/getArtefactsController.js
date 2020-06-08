91e9be22ffc40e1ffbae8b6b0ff797be3060033d
Robert Zanceanu <robert@sprybase.com>
2020-06-08 14:09:39 +0300

    export artefacts in csv and pdf

--- proiect/backend/controllers/artefactsController/getArtefactsController.js
+++ proiect/backend/controllers/artefactsController/getArtefactsController.js
@@ -1,12 +1,12 @@
-const Artefacts = require('../../model/artefactsModel') 
+const Artefacts = require('../../model/artefactsModel')
 const Collections = require('../../model/collectionsModel')
 const ArtefactRarity = require('../../model/rarityModel')
 const ArtefactCondition = require('../../model/conditionModel')
 
 const getArtefactRarity = async (artefact) => {
 
-    return new Promise (async (resolve) => {
-        const artefactRarity =  await ArtefactRarity.find({
+    return new Promise(async (resolve) => {
+        const artefactRarity = await ArtefactRarity.find({
             _id: artefact.rarity
         })
         resolve(artefactRarity[0])
@@ -14,45 +14,45 @@ const getArtefactRarity = async (artefact) => {
 }
 const getArtefactCondition = async (artefact) => {
 
-    return new Promise (async (resolve) => {
-        const artefactCondition =  await ArtefactCondition.find({
+    return new Promise(async (resolve) => {
+        const artefactCondition = await ArtefactCondition.find({
             _id: artefact.condition
         })
         resolve(artefactCondition[0])
     })
 }
-const getCollection = async (artefacts)=>{
+const getCollection = async (artefacts) => {
 
-    return new Promise(async(resolve)=>{
-        const collection = await Collections.find({
-        _id :artefacts.collectionId
-    })
-        resolve(collection[0])
+    return new Promise(async (resolve) => {
+        const collection = await Collections.findOne({
+            _id: artefacts.collectionId
+        })
+        resolve(collection)
     })
 }
-module.exports = async (request,response,userId)=> {
-        let artefacts = await Artefacts.find({userId:userId})
-        let artefactsToSend =[]
-        await Promise.all(artefacts.map(async(artefact)=>{
-            collection = await getCollection(artefact)
-            rarity = await getArtefactRarity(artefact)
-            condition = await getArtefactCondition(artefact)
-            artefactsToSend.push({
-                _id:artefact._id,
-                collection,
-                name:artefact.name,
-                year:artefact.year,
-                value:artefact.value,
-                rarity,
-                condition,
-                description:artefact.description,
-                photos:artefact.photos,
-                numberOfLikes:artefact.numberOfLikes,
-                country:artefact.country,
-                usageHistory:artefact.usageHistory
-            })
-        }))
-        response.writeHead(200, { "Content-Type": "application/json" })
-        response.write(JSON.stringify(artefactsToSend))
-        response.end()
+module.exports = async (request, response, userId) => {
+    let artefacts = await Artefacts.find({})
+    let artefactsToSend = []
+    await Promise.all(artefacts.map(async (artefact) => {
+        const collection = await getCollection(artefact)
+        const rarity = await getArtefactRarity(artefact)
+        const condition = await getArtefactCondition(artefact)
+        artefactsToSend.push({
+            _id: artefact._id,
+            collection:collection,
+            name: artefact.name,
+            year: artefact.year,
+            value: artefact.value,
+            rarity,
+            condition,
+            description: artefact.description,
+            photos: artefact.photos,
+            numberOfLikes: artefact.numberOfLikes,
+            country: artefact.country,
+            usageHistory: artefact.usageHistory
+        })
+    }))
+    response.writeHead(200, { "Content-Type": "application/json" })
+    response.write(JSON.stringify(artefactsToSend))
+    response.end()
 }
