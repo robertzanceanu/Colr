a47e555bce9416f0d2247ea68867e1b758661168
Robert Zanceanu <robert@sprybase.com>
2020-06-11 21:53:27 +0300

    Edit user and signup/login form validation

--- proiect/frontend/signup/signup.js
+++ proiect/frontend/signup/signup.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-    if(localStorage.getItem('auth-token')) {
+    if (localStorage.getItem('auth-token')) {
         window.location.href = '/dashboard'
     }
 })
@@ -32,15 +32,57 @@ submitButton.addEventListener('click', async () => {
     let repeatPassword = document.getElementById('repeat-password').value
     let firstName = document.getElementById('first-name').value
     let lastName = document.getElementById('last-name').value
-    formValues = {
-        email,
-        password,
-        firstName,
-        lastName,
-        role: 'user'
+    let valid = true
+    const fieldErrorEmail = document.getElementById('field-error-email')
+    if (email === '') {
+        valid = false
+        fieldErrorEmail.innerHTML = 'Obligatoriu'
+    } else {
+        fieldErrorEmail.innerHTML = ''
     }
-    let response = await onSubmit(formValues)
-    if (!response.error) {
-        window.location.href = '/login'
+    const fiedErrorLastName = document.getElementById('field-error-lastName')
+    if (lastName === '') {
+        valid = false
+        fiedErrorLastName.innerHTML = 'Obligatoriu'
+    } else {
+        fiedErrorLastName.innerHTML = ''
     }
+    const fieldErrorFirstName = document.getElementById('field-error-firstName')
+    if (firstName === '') {
+        valid = false
+        fieldErrorFirstName.innerHTML = 'Obligatoriu'
+    } else {
+        fieldErrorFirstName.innerHTML = ''
+    }
+    const fieldErrorPassword = document.getElementById('field-error-password')
+    if(password === '') {
+        valid=false
+        fieldErrorPassword.innerHTML = 'Obligatoriu'
+    } else {
+        fieldErrorPassword.innerHTML = ''
+    }
+    const fieldErrorRepeatPassword = document.getElementById('field-error-repeatPassword')
+    if(repeatPassword === '') {
+        valid=false
+        fieldErrorRepeatPassword.innerHTML = 'Obligatoriu'
+    } else if(repeatPassword !== password) {
+        valid=false
+        fieldErrorRepeatPassword.innerHTML = 'Parolele nu coincid'
+    } else {
+        fieldErrorRepeatPassword.innerHTML = ''
+    }
+    if (valid) {
+        formValues = {
+            email,
+            password,
+            firstName,
+            lastName,
+            role: 'user'
+        }
+        let response = await onSubmit(formValues)
+        if (!response.error) {
+            window.location.href = '/login'
+        }
+    }
+
 })
